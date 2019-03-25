2def9c89651eea4751c01943c4953b980cc7f632
akramhelil <akramhelil@gmail.com>
2019-03-25 14:24:16 -0400

    Artist CRUD finished

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,54 @@
 class ArtistsController < ApplicationController
+  before_action :find_artist, only:[:show, :edit, :update, :destroy]
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+    @artist = Artist.new
+
+  end
+
+  def create
+    # byebug
+    @artist = Artist.new(artist_params)
+    if @artist.save
+      redirect_to artist_path(@artist.id)
+    else
+      render new_artist_path
+    end
+  end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+  def edit
+
+    render :edit
+  end
+
+  def update
+
+    @artist.update(artist_params)
+    @artist.save
+    redirect_to artist_path
+  end
+
+  def destroy
+     @artist.destroy
+     @artists = Artist.all
+     render :index
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:first_name, :last_name, :email, :location, :bio)
+  end
+
+  def find_artist
+    @artist = Artist.find(params[:id])
+  end
+
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,10 @@
 class Artist < ApplicationRecord
   has_many :arts
+  validates :first_name, :last_name, :email, presence: true
+
+
+  def fullname
+    self.first_name + " " + self.last_name
+  end
+
 end
