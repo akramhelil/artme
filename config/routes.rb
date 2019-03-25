ca00501a2c5d957f401fc87211dd43ea7d552e9f
akramhelil <akramhelil@gmail.com>
2019-03-25 17:55:02 -0400

    Welcome page setup

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
   resources :arts
   resources :clients
   resources :artists
+
+  root 'welcome#index'
+  get '/about', to: "welcome#about"
+  # get '/clients/login', to: "sessions#client_login"
+  # get '/artists/login', to: "sessions#artist_login"
+  post '/login', to: "sessions#create"
+  post '/logout', to: "sessions#destroy"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
