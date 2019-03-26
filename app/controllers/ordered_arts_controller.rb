75dec4e8a0ef2fd57fa77095b93f5e2f483d5cd4
akramhelil <akramhelil@gmail.com>
2019-03-26 13:33:40 -0400

    added price total amout on the Ordered Art Model

--- app/controllers/ordered_arts_controller.rb
+++ app/controllers/ordered_arts_controller.rb
@@ -1,5 +1,5 @@
 class OrderedArtsController < ApplicationController
-    before_action :find_the_arts, only: :index
+    before_action :find_the_arts, only: [:index, :new]
 
   def index
   #show the items in the shopping cart when hit the
@@ -8,7 +8,7 @@ class OrderedArtsController < ApplicationController
   def new
     cart.each do |art|
       cart_arts = OrderedArt.new(art_id: art.to_i)
-       cart_arts.save(:validate => false)
+      cart_arts.save(:validate => false)
     end
   end
 
