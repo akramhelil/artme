6def86cd776d094f431786c08e481ccd59a30fa2
akramhelil <akramhelil@gmail.com>
2019-03-27 10:48:26 -0400

    Again

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Artist.create(first_name: "Akram", last_name: "Helil", email: "akramhelil@gmail.com", location: "Brooklyn, NY", 
+  bio: "I grewup in Central Asia, learned scatching by my own, after travelling the world I
+  learn that I should start sharing my art with the world, you can also get my information on my instagram or on my website. Thank you! ", password_digest: 12345 )
